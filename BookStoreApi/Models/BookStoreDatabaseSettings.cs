ccea8177864821c2d167993e3dc33b5fa2afaaec
Servin13 <20030367@itcelaya.edu.mx>
2024-03-23 09:56:49 -0600

    Agregar archivos de proyecto.

---
+++ BookStoreApi/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BookStoreApi.Models
+{
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
